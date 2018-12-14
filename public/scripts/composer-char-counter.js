07951b3b04f7afce0059ea9a3b6e2e80f47c1665
Lucy <lucyneatdesign@yahoo.ca>
2018-12-14 20:20:30 +0000

    Final version

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,4 @@
+// Counts symbols typed in the textarea
 $(document).ready(function() {
   $( ".new-tweet form textarea" ).on( "keyup", function() {
     const tweetArea = $(this);
@@ -5,14 +6,12 @@ $(document).ready(function() {
     const counter = tweetArea.siblings("footer").find(".counter");
     counter.text(140 - len);
 
+    // If a tweet too long - counter turns in red
     if (len > 140) {
       counter.addClass("warning");
     } else {
       counter.removeClass("warning");
     }
-
+  });
 });
 
-});
-
-//$(".new-tweet form textarea").text(this);
